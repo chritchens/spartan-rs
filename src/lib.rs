357cc3324b0840bc2778f4c5d5d67bd93d02ee36
chritchens <chritchens@gmail.com>
2019-06-03 18:45:39 +0200

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,32 @@
-use typenum::marker_traits::Unsigned;
 use generic_array::{ArrayLength, GenericArray};
 use curve25519_dalek::scalar::Scalar;
 use std::hash::Hash;
 use std::collections::HashMap;
 
-/// `BitArray` is an array of bits.
-#[derive(Clone, Default, Eq, PartialEq, Ord, PartialOrd, Debug, Hash)]
-pub struct BitArray<N>(GenericArray<u8, N>)
-    where N: ArrayLength<u8>;
-
-/// `Degree` is the degree of a monomial or a polynomial.
-#[derive(Clone, Default, Eq, PartialEq, Ord, PartialOrd, Debug)]
-pub struct Degree<D: Unsigned>(D);
-
 /// `Value` is the a value in the field of order q = 2^255 -19.
 #[derive(Copy, Clone, Default, Eq, PartialEq, Debug)]
 pub struct Value(Scalar);
 
-/// `Variable` is a monomial in the field of order q = 2^255 -19.
-#[derive(Clone, Default, Eq, PartialEq, Debug)]
-pub struct Variable<D>
-    where D: Unsigned
-{
-    pub degree: Degree<D>,
-    pub coefficient: Value,
-    pub value: Option<Value>,
-}
-
-/// `Vector` is a vector of values in the field of order q = 2^255 -19.
-#[derive(Clone, Default, Eq, PartialEq, Debug)]
-pub struct Vector<N>(GenericArray<Value, N>)
-    where N: ArrayLength<Value>;
-
-/// `Polynomial` is a of variables in the field of order q = 2^255 -19.
-#[derive(Clone, Default, Eq, PartialEq, Debug)]
-pub struct Polynomial<D, N>
-    where D: Unsigned,
-          N: ArrayLength<Variable<D>>,
-{
-    pub degree: Degree<D>,
-    pub variables: GenericArray<Variable<D>, N>
-}
+/// `BitArray` is an array of bits.
+#[derive(Clone, Default, Eq, PartialEq, Ord, PartialOrd, Debug, Hash)]
+pub struct BitArray<N>(GenericArray<bool, N>)
+    where N: ArrayLength<bool>;
 
 /// `Label` is a label of a node in the circuit.
 #[derive(Clone, Default, Eq, PartialEq, Ord, PartialOrd, Debug, Hash)]
 pub struct Label<S>(BitArray<S>)
-    where S: Default + Eq + Ord + Hash + ArrayLength<u8>;
+    where S: Default + Eq + Ord + Hash + ArrayLength<bool>;
 
 /// `Labels` is an array of labels of nodes in a Spartan arithmetic circuit.
 #[derive(Clone, Default, Eq, PartialEq, Ord, PartialOrd, Debug, Hash)]
 pub struct Labels<S, L>(GenericArray<Label<S>, L>)
-    where S: Default + Eq + Ord + Hash + ArrayLength<u8>,
+    where S: Default + Eq + Ord + Hash + ArrayLength<bool>,
           L: ArrayLength<Label<S>>;
 
 /// `Op` is an arithmetic circuit operation.
 #[derive(Clone, Eq, PartialEq, Debug)]
 pub enum Op<S>
-    where S: Default + Eq + Ord + Hash + ArrayLength<u8>,
+    where S: Default + Eq + Ord + Hash + ArrayLength<bool>,
 {
     Add { a: Label<S>, b: Label<S>, c: Label<S> },
     Mul { a: Label<S>, b: Label<S>, c: Label<S> },
@@ -65,7 +35,7 @@ pub enum Op<S>
 }
 
 impl<S> Default for Op<S>
-    where S: Default + Eq + Ord + Hash + ArrayLength<u8>,
+    where S: Default + Eq + Ord + Hash + ArrayLength<bool>,
 {
     fn default() -> Op<S> {
         Op::Idx { a: Label::default() }
@@ -76,7 +46,7 @@ impl<S> Default for Op<S>
 /// q = 2^255 -19.
 #[derive(Clone, Default, Eq, PartialEq, Debug)]
 pub struct Node<S>
-    where S: Default + Eq + Ord + Hash + ArrayLength<u8>,
+    where S: Default + Eq + Ord + Hash + ArrayLength<bool>,
 {
     pub label: Label<S>,
     pub op: Op<S>,
@@ -86,7 +56,7 @@ pub struct Node<S>
 /// `Circuit` is an arithmetic circuit in the field of order q = 2^255 -19.
 #[derive(Clone, Default, Debug)]
 pub struct Circuit<S, P, Q, R>
-    where S: Default + Eq + Ord + Hash + ArrayLength<u8>,
+    where S: Default + Eq + Ord + Hash + ArrayLength<bool>,
           P: ArrayLength<Label<S>>,
           Q: ArrayLength<Label<S>>,
           R: ArrayLength<Label<S>>,
